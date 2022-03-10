5ba7092343d476062700ca2729b6423c2ddf4495
Gabriel Soria <gsoriatpe@gmail.com>
2022-03-10 14:17:06 +0000

    first commit

---
+++ server.py
@@ -0,0 +1,51 @@
+#inspired from : https://www.techwithtim.net/tutorials/socket-programming/
+
+import socket # low level implementation
+import sys # for arguments retrieval from terminal
+import threading # threading synchronous processes
+#import module # blockchain module
+import json # encode json objects in block generation
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+class server{
+  
+}
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
+    conn.close()
+        
+
+def run():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        client_socket, client_addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(client_socket, client_addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+run()
