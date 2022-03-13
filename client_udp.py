2e9620eb2af18ec1fcf536fd144d6f9aaf63655d
Neha-224 <73182365+Neha-224@users.noreply.github.com>
2022-03-13 11:51:36 +0000

    Update client_udp.py

--- client_udp.py
+++ client_udp.py
@@ -2,7 +2,9 @@ import socket
 import select
 
 UDP_IP = "127.0.0.1"
+
 IN_PORT = 5005
+
 timeout = 3
 
 
@@ -10,17 +12,17 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, IN_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024)
+    data, addr = sock.recfilesfrm(1024)
     if data:
-        print (f"File name: {data}")
+        print "File name:", data
         file_name = data.strip()
 
-    f = open(file_name, 'wb')
+    f = open(file_name, 'CN')
 
     while True:
         ready = select.select([sock], [], [], timeout)
         if ready[0]:
-            data, addr = sock.recvfrom(1024)
+            data, addr = sock.recfilesfrm(1024)
             f.write(data)
         else:
             print "%s Finish!" % file_name
